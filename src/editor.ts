ba98dee6f4c21277b88cc7d1f20aa4d17c621616
Ian Richardson <iantrich@gmail.com>
2019-03-02 15:52:18 -0600

    fireEvent

--- src/editor.ts
+++ src/editor.ts
@@ -112,6 +112,21 @@ export class BoilerplateCardEditor extends LitElement {
       }
     }
 
-    // fireEvent(this, 'config-changed', { config: this._config });
+    this._fireEvent(this, 'config-changed', { config: this._config }, null);
   }
+
+  private _fireEvent = (node, type, detail, options) => {
+    options = options || {};
+    detail = detail === null || detail === undefined ? {} : detail;
+    const event = new Event(type, {
+      bubbles: options.bubbles === undefined ? true : options.bubbles,
+      cancelable: Boolean(options.cancelable),
+      composed: options.composed === undefined ? true : options.composed
+    }) as any;
+
+    event.detail = detail;
+    node.dispatchEvent(event);
+
+    return event;
+  };
 }
