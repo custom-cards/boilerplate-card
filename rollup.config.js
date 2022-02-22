7d0c2835b869f16649d05de32e09c173a3013eef
Zack <zackbarett@hey.com>
2022-02-22 20:24:24 +0000

    rolup

--- rollup.config.js
+++ rollup.config.js
@@ -30,15 +30,7 @@ const plugins = [
   dev && serve(serveopts),
   !dev && terser(),
   ignore({
-    files: [
-      require.resolve('@material/mwc-notched-outline/mwc-notched-outline.js'),
-      require.resolve('@material/mwc-ripple/mwc-ripple.js'),
-      require.resolve('@material/mwc-menu/mwc-menu.js'),
-      require.resolve('@material/mwc-menu/mwc-menu-surface.js'),
-      require.resolve('@material/mwc-list/mwc-list.js'),
-      require.resolve('@material/mwc-list/mwc-list-item.js'),
-      require.resolve('@material/mwc-icon/mwc-icon.js'),
-    ],
+    files: [...ignoreTextfieldFiles, ...ignoreSelectFiles, ...ignoreSwitchFiles].map((file) => require.resolve(file)),
   }),
 ];
 
